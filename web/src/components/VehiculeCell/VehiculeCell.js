5a42dd0d75ad5f21705978da4bd0a874dabb0565
Do HUYNH <admin@les-detritivores.co>
2022-09-28 15:39:39 +0200

    update tournee component

---
+++ web/src/components/VehiculeCell/VehiculeCell.js
@@ -0,0 +1,28 @@
+import Vehicule from "src/components/Vehicule"
+
+export const QUERY = gql`
+  query FindVehiculeQuery($id: Int!) {
+    vehicule: vehicule(id: $id) {
+      id
+      ordre
+      nom
+      immatriculation
+      identifiant
+      couleur
+      icone
+      actif
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ vehicule }) => {
+  return <Vehicule vehicule={vehicule} />
+}
