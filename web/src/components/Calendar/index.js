de9d9c4a84de44f2bb0af062a2f302a4675ad91c
amineohn <amineprojet7@gmail.com>
2022-09-30 08:50:16 +0200

    feat: use fullCalendar libs

---
+++ web/src/components/Calendar/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import interactionPlugin from '@fullcalendar/interaction'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+const Calendar = () => {
+  const initialEvents = [
+    {
+      title: 'All Day Event',
+      start: new Date(),
+      resourceId: 'b',
+    },
+    {
+      title: 'Long Event',
+      start: new Date(),
+      resourceId: 'c',
+    },
+  ]
+  const initialResources = [
+    { id: 'a', title: 'Auditorium A' },
+    { id: 'b', title: 'Auditorium B', eventColor: 'green' },
+    { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
+  ]
+
+  return (
+    <FullCalendar
+      plugins={[
+        interactionPlugin,
+        resourceTimelinePlugin,
+        dayGridPlugin,
+        timeGridPlugin,
+        listPlugin,
+      ]}
+      initialView="resourceTimelineDay"
+      nowIndicator={true}
+      editable={true}
+      initialEvents={initialEvents}
+      initialResources={initialResources}
+      nowIndicatorContent="Now"
+      dragScroll={true}
+      eventOverlap={false}
+      eventResizableFromStart={true}
+      eventClick={(info) => {
+        console.log(info)
+      }}
+      eventDrop={(info) => {
+        console.log(info)
+      }}
+      eventResize={(info) => {
+        console.log(info)
+      }}
+      drop={(info) => {
+        console.log(info)
+      }}
+      dayMaxEvents={true}
+      eventAdd={(info) => {
+        console.log(info)
+      }}
+      eventRemove={(info) => {
+        console.log(info)
+      }}
+      eventChange={(info) => {
+        console.log(info)
+      }}
+    />
+  )
+}
+export default Calendar
